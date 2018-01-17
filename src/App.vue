5f90e366eac32eff0fe0e3451d514894225c9e94
AtsunaAkiya <atsunaakiya@outlook.com>
2018-01-17 14:06:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,227 @@
+<template lang="jade">
+#app
+  h1.center 驿宁地点信息生成器
+  .container
+    template(v-if="from_exists_doc")
+      .row
+        form.col.s12
+          .file-field.input-field
+            .btn
+              span 上传文件
+              input(type="file", @change="upload_file($event)")
+            .file-path-wrapper
+              input.file-path.validate(type="text")
+    .row(v-else)
+      span.col.s8 您正在创建一份新文档, 但您也可以
+      button.col.s4.btn(@click="from_exists_doc = true") 从现有文件开始编辑
+    .row
+      .input-field.col.s8
+        input#title_input(v-model="title", type="text")
+        label(for="title_input") 地点名称
+      .input-field.col.s4
+        select#loctype_select
+          option(value="unknown") 未知地点(请勿选择)
+          option(value="pub")     公共场所
+          option(value="gov")     政府机构
+          option(value="party")   政党财产
+          option(value="com")     商业地产
+          option(value="pri")     个人房屋(请勿宣告所有权)
+        label 地点类别
+    .row
+      .input-field.col.s6
+        input#username_input(v-model="username", type="text")
+        label(for="username_input") 尊姓大名
+      .input-field.col.s6
+        | 其他作者:
+        span(v-for="a in authors")
+          | {{a}}
+    .row
+      button.col.s2.btn(@click="save_file") 导出
+      span.col.s2 Ver {{version.toFixed(1)}}
+      span.col.s4(@click="save_now")
+        span(v-if="autosave_time")
+          | 已保存于 {{format_date(autosave_time)}}
+        span(v-else)
+          | 尚未保存
+      button.col.s2.btn.blue(@click="add_title") 添加段落标题
+      button.col.s2.btn.blue(@click="add_link") 添加链接
+    .row
+      .col.s12
+        ul.tabs
+          li.tab.col.s6
+            a.active(href="#tab_edit") 编辑
+          li.tab.col.s6
+            a(href="#tab_preview") 预览
+        .input-field#tab_edit
+          textarea#md-text.materialize-textarea(v-model="markdown")
+          label(for="md-text") 输入文字
+        div#tab_preview(v-html="markdown_preview(markdown)")
+</template>
+
+<script>
+import FileSaver from 'file-saver'
+import YAML from 'js-yaml'
+import marked from 'marked'
+const ALL_TYPES = [
+  'unknown',
+  'pub',
+  'gov',
+  'party',
+  'com',
+  'pri',
+]
+export default {
+  data(){
+    return {
+      from_exists_doc: false,
+      version: 0,
+      loctype: 'unknown',
+      authors: [],
+      username: '',
+      // tags: [],
+      title:    '',
+      markdown: '',
+      autosave_time: null
+    }
+  },
+  methods: {
+    upload_file(evt){
+      if(evt.target.files.length == 0){
+        return
+      }
+      const file = evt.target.files[0]
+      const reader = new FileReader()
+      reader.onload = (evt) => {
+        const result = evt.target.result
+        const [meta, markdown] = YAML.safeLoadAll(result)
+        const v_md = typeof(markdown) === "string"
+        const {
+          title,
+          authors,
+          version,
+          type,
+          // tags
+        } = meta
+        const v_authors = Array.isArray(authors)
+        // const v_tags = Array.isArray(tags)
+        const v_version = typeof(version) === "number" && version >= 0
+        const v_type = ALL_TYPES.includes(type)
+        if(
+            !v_md || 
+            !v_authors || 
+            // !v_tags || 
+            !v_version 
+            || !v_type){
+          alert("非法文件格式")
+          return
+        }
+        this.title =    title
+        this.authors =  authors
+        this.version =  version + 1
+        // this.loctype =     type
+        $("#loctype_select").val(type)
+        // this.tags =     tags
+        this.markdown = markdown
+      }
+      reader.readAsText(file)
+    },
+    save_file(){
+      let type = $("#loctype_select").val()
+      if(!this.username){
+        alert("您还没填写名字")
+        return
+      }
+      if(!this.title){
+        alert("您还没填写地点名")
+        return
+      }
+      if(type === 'unknown'){
+        alert("不可以生成未知类型的地点")
+        return
+      }
+      if(!this.markdown){
+        alert("不可以生成空文件")
+        return
+      }
+      console.log(type)
+      let authors = []
+      for(let a of this.authors){
+        if(a != this.username){
+          authors.push(a)
+        }
+      }
+      authors.push(this.username)
+      const meta = {
+        title: this.title,
+        authors,
+        type,
+        version: this.version
+      }
+      const date = new Date
+      const text = this.preproc(this.markdown)
+      const content = "---\n" + YAML.safeDump(meta) + "---\n" + YAML.safeDump(text)
+      const filename = `${this.username}_${this.title}_v${this.version}.ymd`
+      const blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+      FileSaver.saveAs(blob, filename);
+    },
+    preproc(text){
+      return text.split(/\n+/).join("\n\n")
+    },
+    markdown_preview(text){
+      return marked(this.preproc(text))
+    },
+    add_title(){
+      this.markdown += "\n\n## 在此处添加段落标题\n"
+      $("#tab_edit").val(this.markdown)
+    },
+    add_link(){
+      this.markdown += "\n\n[链接显示文字](http://链接地址)\n"
+      $("#tab_edit").val(this.markdown)
+    },
+    format_date(date){
+      let d = new Date
+      let timesep = (d.getTime() - date.getTime()) / 1000
+      return `${(timesep / 60).toFixed(1)}分钟 前`
+    },
+    save_now(){
+      localStorage['yipolis.map.editor.backup'] = this.markdown
+      this.autosave_time = new Date
+    }
+  },
+  mounted(){
+    $(document).ready(function() {
+      $('select').material_select();
+      $('ul.tabs').tabs('select_tab', 'tab_edit');
+    
+      $("#loctype_select").val('unknown')
+    });
+  },
+  created(){
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      // pass
+    } else {
+      alert('您的浏览器无法支持文件上传功能')
+    }
+    this.markdown = localStorage['yipolis.map.editor.backup'] || ''
+    setInterval(() => {
+      if(this.markdown){
+        localStorage['yipolis.map.editor.backup'] = this.markdown
+        this.autosave_time = new Date
+      }
+    }, 90 * 1000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  font-size: 1.5rem;
+}
+#tab_edit{
+  min-height: 40rem;
+}
+</style>
